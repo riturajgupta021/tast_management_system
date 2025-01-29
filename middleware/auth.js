d0c90b788eb45da9848071971b87cde4000d689d
riturajgupta021 <riturajgupta021@gmail.com>
2025-01-29 23:20:34 +0530

    remove console logs

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
     const token = req.header('Authorization');
-  console.log(token)
   if (!token || !token.startsWith('Bearer ')) {
     return res.status(401).json({ error: 'Access denied. No token provided.' });
   }
