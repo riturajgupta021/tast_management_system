5b0893d8e1a35baaba20e42eacc1d41ffd7c5461
Rituraj Gupta <riturajgupta021@gmail.com>
2025-01-30 14:55:15 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 function dbConnection() {
-    mongoose.connect('mongodb://localhost:27017/task-manager').then(() => {
+    mongoose.connect(Mong_url).then(() => {
         console.log("Connected to database");
     }).catch((err) => {
         console.log("Error connecting to database", err.message);
